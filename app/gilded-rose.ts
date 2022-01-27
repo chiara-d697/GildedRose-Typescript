a965fa8d1aca2820e73d692b7107bbf2f4bff83b
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-27 15:40:05 +0000

    final formatting tweaks

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,32 +18,32 @@ export class GildedRose {
     }
 
     checkQualityBounds(item) {
-        if(item.quality <= 50 && item.quality >= 0) { return true}
+        if (item.quality <= 50 && item.quality >= 0) { return true }
     }
 
     updateItemQuality(item) {
-        if(this.checkQualityBounds(item)) {
-            if(item.sellIn > 0) {
-                item.sellIn --;
-                item.quality --;
-                
-            }  else {
-                item.sellIn --;
-                item.quality -= 2; 
+        if (this.checkQualityBounds(item)) {
+            if (item.sellIn > 0) {
+                item.sellIn--;
+                item.quality--;
+
+            } else {
+                item.sellIn--;
+                item.quality -= 2;
 
             }
         }
     }
 
     updateBrieQuality(brie) {
-        if(this.checkQualityBounds(brie)) {
-            if(brie.sellIn > 0) {
-                brie.sellIn --;
-                brie.quality ++;
-                
-            }  else {
-                brie.sellIn --;
-                brie.quality += 2; 
+        if (this.checkQualityBounds(brie)) {
+            if (brie.sellIn > 0) {
+                brie.sellIn--;
+                brie.quality++;
+
+            } else {
+                brie.sellIn--;
+                brie.quality += 2;
             }
         }
 
@@ -55,52 +55,50 @@ export class GildedRose {
     }
 
     updatePassQuality(pass) {
-        if(this.checkQualityBounds(pass)) {
-            if(pass.sellIn <= 0) {
+        if (this.checkQualityBounds(pass)) {
+            if (pass.sellIn <= 0) {
                 pass.quality = 0;
-            } else if(pass.sellIn <= 5) {
+            } else if (pass.sellIn <= 5) {
                 pass.quality += 3;
-            } else if(pass.sellIn <= 10) {
+            } else if (pass.sellIn <= 10) {
                 pass.quality += 2;
             } else {
-                pass.quality ++;
+                pass.quality++;
             }
-            pass.sellIn --;
-            }
-        } 
+            pass.sellIn--;
+        }
+    }
 
-        updateConjuredItemQuality(item) {
-            if(this.checkQualityBounds(item)) {
-                if(item.sellIn > 0) {
-                    item.sellIn --;
-                    item.quality -= 2;
-                    
-                }  else {
-                    item.sellIn --;
-                    item.quality -= 4; 
-    
-                }
+    updateConjuredItemQuality(item) {
+        if (this.checkQualityBounds(item)) {
+            if (item.sellIn > 0) {
+                item.sellIn--;
+                item.quality -= 2;
+
+            } else {
+                item.sellIn--;
+                item.quality -= 4;
+
             }
         }
+    }
 
-        updateQuality() {
-        
-
-            for(const i of this.items) {
-                if(i.name === 'Conjured Mana Cake') {
-                    this.updateConjuredItemQuality(i);
-                } else if(i.name === 'Aged Brie') {
-                    this.updateBrieQuality(i);
-                } else if(i.name === 'Sulfuras, Hand of Ragnaros') {
-                    this.updateSulfurasQuality(i);
-                } else if(i.name === 'Backstage passes to a TAFKAL80ETC concert') {
-                    this.updatePassQuality(i);
-                } else {
-                    this.updateItemQuality(i);
-                }
-    
+    updateQuality() {
+        for (const i of this.items) {
+            if (i.name === 'Conjured Mana Cake') {
+                this.updateConjuredItemQuality(i);
+            } else if (i.name === 'Aged Brie') {
+                this.updateBrieQuality(i);
+            } else if (i.name === 'Sulfuras, Hand of Ragnaros') {
+                this.updateSulfurasQuality(i);
+            } else if (i.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                this.updatePassQuality(i);
+            } else {
+                this.updateItemQuality(i);
             }
-    
-            return this.items;
+
+        }
+
+        return this.items;
     }
 }
